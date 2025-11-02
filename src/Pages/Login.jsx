cb630951cdd6ba157ee244f120161804c08be36a
man <manp64051@gmail.com>
2025-11-02 18:11:44 +0530

    init

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "./Auth.css";
+import "./Auth.css"
 
 export default function AuthPage() {
   const [view, setView] = useState("login");
